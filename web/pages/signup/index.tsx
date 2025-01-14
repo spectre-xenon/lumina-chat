92287bcd7143e668ea27f14778f671a2f4a2baf4
Spectre-Xenon <62655598+spectre-xenon@users.noreply.github.com>
2025-01-14 13:51:11 +0200

    feat(web): added signup page

---
+++ web/pages/signup/index.tsx
@@ -0,0 +1,155 @@
+import {
+  Dispatch,
+  FormEvent,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
+import { toast } from "sonner";
+import { Link, useLocation, useSearch } from "wouter";
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+import { simpleFetch } from "~/hooks/useFetch";
+import { apiCodesMap, genericErrorsMap } from "~/lib/statuscodes";
+import { ApiResponse } from "~/types/api";
+import { User } from "~/types/user";
+
+export function SignupPage({
+  setAuthed,
+}: {
+  setAuthed: Dispatch<SetStateAction<boolean>>;
+}) {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [, navigate] = useLocation();
+  const params = useSearch();
+
+  useEffect(() => {
+    const errcode = new URLSearchParams(params).get("errcode");
+
+    if (errcode) {
+      requestAnimationFrame(() =>
+        toast.error(apiCodesMap[Number(errcode)], {
+          position: "top-center",
+        }),
+      );
+    }
+  }, [params]);
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    // Generic checks
+    if (username === "" || email === "" || password === "")
+      return toast.error(genericErrorsMap["emptyFormField"]);
+
+    if (username.length < 3)
+      return toast.error(genericErrorsMap["shortUsername"]);
+
+    if (password.length < 8)
+      return toast.error(genericErrorsMap["shortPassword"]);
+
+    const body = new URLSearchParams({
+      username,
+      email,
+      password,
+    });
+
+    const data = await simpleFetch<ApiResponse<User>>(
+      "/v1/auth/signup",
+      navigate,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: body,
+      },
+    );
+
+    // if Error do nothing and notify
+    if (data.err_code) {
+      return toast.error(apiCodesMap[data.err_code], {
+        position: "top-center",
+      });
+    }
+
+    // Success go home
+    setAuthed(true);
+    navigate("/");
+  }
+
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-6">
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">Signup</CardTitle>
+          <CardDescription>
+            Enter your email below to create a new account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  id="username"
+                  name="username"
+                  type="username"
+                  placeholder="jhin"
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  name="password"
+                  type="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <Button type="submit" className="w-full">
+                Signup
+              </Button>
+              <Button
+                variant="outline"
+                className="w-full"
+                onClick={() => {
+                  window.open("/v1/auth/login/google", "_self");
+                }}
+              >
+                Login with Google
+              </Button>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Already have an account?{" "}
+              <Link to="/login" className="underline underline-offset-4">
+                login
+              </Link>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
--- web/router.tsx
+++ web/router.tsx
@@ -3,6 +3,7 @@ import { useAuth } from "~/hooks/useAuth";
 import { NotFound } from "./pages/404";
 import { LoginPage } from "./pages/login";
 import { Toaster } from "./components/ui/sonner";
+import { SignupPage } from "./pages/signup";
 
 const allowedLocations: { [key: string]: boolean } = {
   "/login": true,
@@ -32,7 +33,9 @@ export function Router() {
       <Switch>
         <Route path="/">home</Route>
         <Route path="/login">{() => <LoginPage setAuthed={setAuthed} />}</Route>
-        <Route path="/signup" />
+        <Route path="/signup">
+          {() => <SignupPage setAuthed={setAuthed} />}
+        </Route>
 
         {/* Default route in a switch */}
         <Route component={NotFound}></Route>
